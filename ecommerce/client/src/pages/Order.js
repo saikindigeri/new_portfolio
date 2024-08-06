241ee8ca7841deba47b241c64ae8ee20925a1509
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2024-08-06 14:32:38 +0530

    chagned a

--- ecommerce/client/src/pages/Order.js
+++ ecommerce/client/src/pages/Order.js
@@ -41,7 +41,9 @@ const Orders = () => {
                                 <p className="card-text">Quantity: {order.quantity}</p>
                                 <p className="card-text">Price: Rs {order.price}</p>
                             </div>
-                            <img src={order.image_url} alt="image" className="bgimage me-3" style={{ width: '100px' }} />
+                         <div>
+                            <img src={order.image_url} alt="image" />
+                         </div>
                         </div>
                     ))}
                 </div>
